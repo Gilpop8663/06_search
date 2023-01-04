c4316c0c35b1b0f394cb73f1355e7af157bcaeca
gilpop8663 <wolfye@naver.com>
2023-01-05 08:12:05 +0900

    fix(api): 기존 heroku 서버에서 cloudtype 서버로 이동

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,8 @@ export interface medicineDataProps {
   brand?: string;
 }
 
-const BASE_URL = 'https://dry-hollows-03672.herokuapp.com/medicine';
+const BASE_URL =
+  'https://port-0-node-express-3vw25lci3y9wi.gksl2.cloudtype.app/medicine';
 
 export const getData = () => {
   return fetch(BASE_URL).then((res) => res.json());
